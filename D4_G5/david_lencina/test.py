ebbb49f89deac549846d2a7b822742f016dbff09
LDA407 <88114886+LDA407@users.noreply.github.com>
2023-05-19 18:54:53 -0300

    elimine las carpetas para evitar la sobreescritura de las demas

--- D4_G5/david_lencina/test.py
+++
@@ -1,9 +0,0 @@
-from models.inmobiliario import Inmueble
-
-
-if __name__ == '__main__':
-    p1 = Inmueble(año=2010, metros=255, habitaciones=6, garage=True, zona="A", estado="Disponible")
-    p2 = Inmueble(año=2000, metros=40, habitaciones=3, garage=False, zona="B", estado="Disponible")
-    
-    print(p1)
-    print(p2.__str__())
